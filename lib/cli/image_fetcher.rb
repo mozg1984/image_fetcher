32f5b8a90991ce09ad3762ccc6cbe5d63312f369
Радик Хисамутдинов <mozg1984@gmail.com>
2021-03-26 14:16:40 +0400

    Replaced whole downloading to chunked downloading

--- lib/cli/image_fetcher.rb
+++ lib/cli/image_fetcher.rb
@@ -2,10 +2,19 @@
 
 require 'down'
 require 'fileutils'
+require 'securerandom'
 require './lib/cli/settings_accessible'
 
 module CLI
   class ImageFetcher
+    CHUNK_SIZE = 1024 # 1KB
+
+    class Error < StandardError; end
+
+    class TooLargeImage < Error; end
+
+    class UnknownContentType < Error; end
+
     include CLI::SettingsAccessible
     attr_reader :url, :destination
 
@@ -15,17 +24,52 @@ module CLI
     end
 
     def fetch
-      temp_image = Down.download(
-        url,
-        max_redirects: settings[:max_redirects],
-        max_size: settings[:max_size]
-      )
-
-      FileUtils.mv(temp_image.path, "#{destination}/#{temp_image.original_filename}")
-    rescue Down::Error => e
+      fetch!
+    rescue ImageFetcher::Error, Down::Error, Errno::ENOENT, Errno::EACCES => e
       puts "An error occurred while downloading the file #{url}: #{e.message}"
+    end
+
+    def fetch!
+      remote_image = Down.open(url, **settings[:down])
+
+      check_size!(remote_image)
+      check_content_type!(remote_image)
+
+      image_file = File.new(local_filename_for(remote_image), 'wb')
+
+      loop do
+        break if remote_image.eof?
+
+        image_file.write(remote_image.read(CHUNK_SIZE))
+      end
     ensure
-      temp_image&.close
+      remote_image&.close
+      image_file&.close
+    end
+
+    private
+
+    def local_filename_for(remote_image)
+      extension = content_type(remote_image).split('/')[1]
+      extension = ".#{extension}" unless extension.empty?
+      "#{destination}/#{SecureRandom.urlsafe_base64}#{extension}"
+    end
+
+    def content_type(remote_image)
+      @content_type ||= remote_image.data&.[](:headers)&.[]('Content-Type')&.to_s
+    end
+
+    def check_size!(remote_image)
+      max_size = settings[:down][:max_size]
+      return if remote_image.size <= max_size
+
+      raise ImageFetcher::TooLargeImage, "file is too large (max is #{max_size / 1024 / 1024}MB)"
+    end
+
+    def check_content_type!(remote_image)
+      return if settings[:down][:content_type].include?(content_type(remote_image))
+
+      raise ImageFetcher::UnknownContentType, "file contains unknown content type #{content_type(remote_image)}"
     end
   end
 end
--- spec/image_fetcher_spec.rb
+++ spec/image_fetcher_spec.rb
@@ -5,20 +5,128 @@ require 'cli/image_fetcher'
 RSpec.describe CLI::ImageFetcher do
   let(:url) { 'http://localhost/test.png' }
   let(:destination) { '.' }
+  let(:size) { 1 * 1024 * 1024 }
+  let(:content_type) { 'image/png' }
+  let(:data) { { headers: { 'Content-Type' => content_type } } }
+  let(:remote_image_file_mock) { spy('Down::ChunkedIO') }
+  let(:image_file_mock) { spy('File') }
+  let(:image_file_content_mock) { 'Some binary image data' }
 
   subject(:image_fetcher) { described_class.new(url, destination) }
 
-  describe '#fetch' do
-    let(:temp_file_mock) { spy(:temp_file) }
+  def stub_transfering_data
+    allow(Down).to receive(:open).and_return(remote_image_file_mock)
+    allow(File).to receive(:new).and_return(image_file_mock)
+    allow(remote_image_file_mock).to receive(:size).and_return(size)
+    allow(remote_image_file_mock).to receive(:data).and_return(data)
+    allow(remote_image_file_mock).to receive(:read).and_return(image_file_content_mock)
+    allow(remote_image_file_mock).to receive(:eof?).and_return(false, true)
+  end
 
-    before do
-      allow(Down).to receive(:download).and_return(temp_file_mock)
-      allow(FileUtils).to receive(:mv)
+  describe '#fetch' do
+    context 'when url is empty' do
+      let(:url) { '' }
+
+      it 'does not raise any error' do
+        expect { image_fetcher.fetch }.not_to raise_error
+      end
     end
 
-    it 'calls Down downloader' do
-      expect(Down).to receive(:download)
-      image_fetcher.fetch
+    context 'when url is incorrect' do
+      let(:url) { 'oops://oops' }
+
+      it 'does not raise any error' do
+        expect { image_fetcher.fetch }.not_to raise_error
+      end
+    end
+
+    context 'when url is correct' do
+      before { stub_transfering_data }
+
+      context 'when size is too large' do
+        let(:size) { 10 * 1024 * 1024 }
+
+        before { allow(remote_image_file_mock).to receive(:size).and_return(size) }
+
+        it 'does not raise any error' do
+          expect { image_fetcher.fetch }.not_to raise_error
+        end
+      end
+
+      context 'when content_type is missing' do
+        let(:data) { { headers: {} } }
+
+        it 'does not raise any error' do
+          expect { image_fetcher.fetch }.not_to raise_error
+        end
+      end
+
+      context 'when content_type is unknown' do
+        let(:content_type) { 'image/unknown' }
+
+        it 'does not raise any error' do
+          expect { image_fetcher.fetch }.not_to raise_error
+        end
+      end
+
+      it 'delegates call to unsafe fetching' do
+        expect(image_fetcher).to receive(:fetch!)
+        image_fetcher.fetch
+      end
+    end
+  end
+
+  describe '#fetch!' do
+    context 'when url is empty' do
+      let(:url) { '' }
+
+      it 'raises Down::Error error' do
+        expect { image_fetcher.fetch! }.to raise_error(Down::Error)
+      end
+    end
+
+    context 'when url is incorrect' do
+      let(:url) { 'oops://oops' }
+
+      it 'raises Down::Error error' do
+        expect { image_fetcher.fetch! }.to raise_error(Down::Error)
+      end
+    end
+
+    context 'when url is correct' do
+      before { stub_transfering_data }
+
+      context 'when size is too large' do
+        let(:size) { 10 * 1024 * 1024 }
+
+        before { allow(remote_image_file_mock).to receive(:size).and_return(size) }
+
+        it 'raises TooLargeImage error' do
+          expect { image_fetcher.fetch! }.to raise_error(described_class::TooLargeImage)
+        end
+      end
+
+      context 'when content_type is missing' do
+        let(:data) { { headers: {} } }
+
+        it 'raises UnknownContentType error' do
+          expect { image_fetcher.fetch! }.to raise_error(described_class::UnknownContentType)
+        end
+      end
+
+      context 'when content_type is unknown' do
+        let(:content_type) { 'image/unknown' }
+
+        it 'raises UnknownContentType error' do
+          expect { image_fetcher.fetch! }.to raise_error(described_class::UnknownContentType)
+        end
+      end
+
+      it 'transfers binary data by chunks' do
+        expect(remote_image_file_mock).to receive(:read).with(described_class::CHUNK_SIZE)
+        expect(image_file_mock).to receive(:write).with(image_file_content_mock)
+        image_fetcher.fetch!
+      end
     end
   end
 end
